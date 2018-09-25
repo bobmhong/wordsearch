7698f5c2afec0d96de1f7d07f558fc71480d6671
Bob Hong <bobmhong@gmail.com>
2018-09-25 18:33:48 -0400

    handle the way windows returns full paths

--- src/test/java/com/bobmhong/kata/wordsearchTest.java
+++ src/test/java/com/bobmhong/kata/wordsearchTest.java
@@ -15,7 +15,11 @@ public class wordsearchTest
         String wsInputFileName = "trekSampleData.txt";
         ClassLoader classLoader = wordsearchTest.class.getClassLoader();
         String wsInputFileNameFullPath = classLoader.getResource(wsInputFileName).getFile();
-
+        
+        if (wsInputFileNameFullPath.contains(":")) {
+            wsInputFileNameFullPath = wsInputFileNameFullPath.substring(1);
+        }
+        
         wordsearch ws = new wordsearch();
         ArrayList<String> wsInputArrayList = ws.LoadWordSearchInputArrayList(wsInputFileNameFullPath);
         assertEquals("BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA", wsInputArrayList.get(0));
