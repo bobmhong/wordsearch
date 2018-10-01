cb8f782fb6182ddbbcf49ec8ede0c22e51abe346
Bob Hong <bobmhong@gmail.com>
2018-10-01 12:32:09 -0400

    added searchVector tests and logic

---
+++ src/main/java/com/bobmhong/kata/SearchVector.java
@@ -0,0 +1,58 @@
+package com.bobmhong.kata;
+import java.util.ArrayList;
+import java.util.regex.*;  
+
+public class SearchVector {
+    private ArrayList<Cell> cells;
+    
+    public SearchVector() {
+        this.cells = new ArrayList<Cell>();
+    }
+
+    private void addCell(Cell c) {
+        this.cells.add(c);
+    }
+
+    public void addCell(String cellText, int x, int y){
+        Cell c = new Cell(cellText, x, y);
+        this.cells.add(c);
+    }
+    
+    public String getMatchCoordinates(String searchWord)
+    {
+        String coordString = "";
+
+        String vectorString = this.toString();
+
+        Pattern p = Pattern.compile(searchWord);
+        Matcher m = p.matcher(vectorString);  
+        
+        if (m.find()){
+            coordString = searchWord + ": ";
+            for (int i = m.start(); i < m.end(); i++ ) {
+
+                if (i > m.start()) {
+                    coordString += ",";
+                }
+
+                coordString += this.cells.get(i).getCoordinate();
+            }
+        }
+
+        return coordString;
+    }
+
+    public int size(){
+        return cells.size();
+    }
+
+    @Override
+    public String toString(){
+        String vectorString = "";
+        for (Cell c : this.cells) {
+            vectorString += c.toString();
+        }
+        return vectorString;
+    }
+    
+}
---
+++ src/test/java/com/bobmhong/kata/SearchVectorTest.java
@@ -0,0 +1,48 @@
+package com.bobmhong.kata;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.Ignore;
+
+public class SearchVectorTest{
+    @Test
+    public void testAddCell() {
+        SearchVector sv = new SearchVector();
+        sv.addCell("A", 1, 0);
+        assertEquals(1, sv.size());
+    }
+
+    @Test
+    public void testGetMatchCoordinates() {
+
+        String searchWord = "DOG";
+
+        SearchVector sv = new SearchVector();
+        sv.addCell("A", 1, 0);
+        sv.addCell("S", 2, 0);
+        sv.addCell("X", 3, 0);
+        sv.addCell("D", 4, 0);
+        sv.addCell("O", 5, 0);
+        sv.addCell("G", 6, 0);
+        sv.addCell("P", 7, 0);
+
+        String coordString = sv.getMatchCoordinates(searchWord);
+
+        assertEquals("DOG: (4,0),(5,0),(6,0)", coordString);
+
+    }
+
+    @Test
+    public void testToString() {
+
+        SearchVector sv = new SearchVector();
+        sv.addCell("A", 1, 0);
+        sv.addCell("S", 2, 0);
+        sv.addCell("X", 3, 0);
+
+        String vectorString = sv.toString();
+
+        assertEquals("ASX", sv.toString());
+
+    }
+}
