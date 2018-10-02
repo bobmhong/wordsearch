6ec3572d271ba63283632b587b21bff5ddf4fc99
Bob Hong <bobmhong@gmail.com>
2018-10-01 23:47:27 -0400

    only attempt to load sample file if param not passed

--- src/main/java/com/bobmhong/kata/App.java
+++ src/main/java/com/bobmhong/kata/App.java
@@ -3,20 +3,27 @@ package com.bobmhong.kata;
 public class App {
 	public static void main(String[] args) {
 		String wsInputFileName;
+		String wsInputFileNameFullPath;
 
 		if (args.length > 0) {
-			wsInputFileName = args[0];
+			wsInputFileNameFullPath = args[0];
 		} else {
+			// use sample file if none is specified
 			wsInputFileName = "trekSampleData.txt";
+			ClassLoader classLoader = App.class.getClassLoader();
+			wsInputFileNameFullPath = classLoader.getResource(wsInputFileName).getFile();
+
+			System.out.println("Original File Attempting to load: " + wsInputFileNameFullPath);
+
+			// Strip leading / in Windows which returns path like /c:/mypath
+			if (wsInputFileNameFullPath.contains(":")) {
+				wsInputFileNameFullPath = wsInputFileNameFullPath.substring(1);
+			}
+
 		}
 
-		ClassLoader classLoader = App.class.getClassLoader();
-		String wsInputFileNameFullPath = classLoader.getResource(wsInputFileName).getFile();
+		System.out.println("Attempting to load: " + wsInputFileNameFullPath);
 
-		// Strip leading / in Windows which returns path like /c:/mypath
-		if (wsInputFileNameFullPath.contains(":")) {
-			wsInputFileNameFullPath = wsInputFileNameFullPath.substring(1);
-		}
 		Wordsearch ws = new Wordsearch();
 
 		ws.init(wsInputFileNameFullPath);
